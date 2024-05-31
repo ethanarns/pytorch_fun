0604af481cbb08e371670f2ff394145534f1525c
Ethan Messinger-Arns <ethanarns95@gmail.com>
2024-05-31 16:14:59 -0700

    All parameter named

--- better_convolutional_neural_network.py
+++ better_convolutional_neural_network.py
@@ -10,38 +10,58 @@ if not torch.cuda.is_available():
     print("WARNING: Cuda not available! Calculations will be slower!")
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Hyper-parameters 
+# Input data
+COLOR_CHANNELS = 1
+POOL_DIMS = 2
+IMG_DIMS = 28
+CLASSES = 10 # 0-9
+# The image is 2x2 pooled twice
+IMG_POOLED_DIMS = int(IMG_DIMS / 2 / 2)
+
+# Hyper-parameters
 NUM_EPOCHS = 10
 BATCH_SIZE = 32
 LEARNING_RATE = 0.001
+FILTERS_1 = 32 # Can be adjusted custom
+FILTERS_2 = 64 # Can be adjusted custom
+FC_FEATURES = 128 # Can be adjusted custom
+FILTER_DIMENSION = 3 # 3x3 filter walked across image
+FILTER_PADDING = 1
 
 class ConvNeuralNetwork(nn.Module):
     def __init__(self) -> None:
         super(ConvNeuralNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(1,32,kernel_size=3,padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(64*7*7,128)
-        self.fc2 = nn.Linear(128,10)
+        self.pool = nn.MaxPool2d(POOL_DIMS,POOL_DIMS)
         self.relu = nn.ReLU()
+
+        self.conv1 = nn.Conv2d(COLOR_CHANNELS,FILTERS_1,kernel_size=FILTER_DIMENSION,padding=FILTER_PADDING)
+        self.conv2 = nn.Conv2d(FILTERS_1, FILTERS_2, kernel_size=FILTER_DIMENSION, padding=FILTER_PADDING)
+        self.fc1 = nn.Linear(FILTERS_2*IMG_POOLED_DIMS*IMG_POOLED_DIMS,FC_FEATURES)
+        self.fc2 = nn.Linear(FC_FEATURES,CLASSES)
     
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1,64 * 7 * 7)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        # x's shape starts as [32, 1, 28, 28]
+        # The MNIST images are 28x28 px
+
+        # The 2x2 pool samples down the 28x28 pixel images into 14x14
+        x = self.pool(self.relu(self.conv1(x))) # Makes shape [32, 32, 14, 14]
+        # The 2x2 pool samples down the 14x14 pixel images into 7x7
+        x = self.pool(self.relu(self.conv2(x))) # Makes shape [32, 64, 7, 7]
+        x = x.view(-1,FILTERS_2 * IMG_POOLED_DIMS * IMG_POOLED_DIMS) # Changes the shape to [32, 3136]
+        x = self.relu(self.fc1(x)) # Changes the shape to [32, 128]
+        x = self.fc2(x) # Changes the shape to [32, 10]
         return x
     
 # Pre-process the data
-# transform will be used to make the loaded datasets easier to read
+# transform will be used to make the loaded datasets easier to use
 transform = transforms.Compose([
-    transforms.Resize((28,28)),
+    transforms.Resize((IMG_DIMS,IMG_DIMS)),
     transforms.ToTensor(),
     transforms.Normalize((0.5,),(0.5,))
 ])
 
-# Load MNIST
+print("Loading datasets...")
+# Load MNIST (28x28 pix images)
 train_dataset = MNIST(root='./data', train=True, download=True, transform=transform)
 test_dataset = MNIST(root='./data', train=False, download=True, transform=transform)
 # Place into loaders
@@ -55,6 +75,8 @@ model = ConvNeuralNetwork().to(DEVICE)
 criterion = nn.CrossEntropyLoss()
 # Optimizer
 optimizer = optim.Adam(model.parameters(),lr=LEARNING_RATE)
+
+print("Starting training...")
 
 n_total_steps = len(train_loader)
 for epoch in range(NUM_EPOCHS):
